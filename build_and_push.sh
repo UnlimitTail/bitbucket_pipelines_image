67e4b2370ef43d0565ba349e12e3707e3398fc04
Jake Yang <jakeyang@Jakeui-MacBook-Pro.local>
2019-02-09 01:43:24 +0900

    refac

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-VERSION=0.0.1
+VERSION=0.0.2
 TAG=utail/bitbucket_pipelines_image
 
-docker build --tag=$TAG:$VERSION .
-docker push $TAG:$VERSION
-docker push $TAG:latest
+docker build --tag=$TAG:$VERSION . && docker push $TAG:$VERSION
